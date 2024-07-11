07c073f6e087476704b20684f9c92c6ee7e5dd57
cskonopka <cskonopka@gmail.com>
2024-07-11 11:18:37 -0400

    fixed double_cup and bad_predator

--- programs/bad_predator.py
+++ programs/bad_predator.py
@@ -1,9 +1,9 @@
 from utils.ffmpeg_operations import run_ffmpeg_command
 
-def bad_predator(input_file1, input_file2, output_file):
+def bad_predator(input_file, output_file):
     command = [
         "ffmpeg",
-        "-i", input_file1,
+        "-i", input_file,
         "-filter_complex", "[0:v]shuffleplanes,smartblur=luma_radius=1.6:lr=2.88:luma_strength=0.11:lt=-17:cr=0.93,amplify=radius=33:factor=4:threshold=10721.12:tolerance=12:low=12420.26:high=49802.12,despill=mix=0.58:expand=0.85:red=-1.2:green=1.21:blue=2.3:brightness=4.45:alpha=true,lagfun=decay=0.85[out_v]",
         "-map", "[out_v]",
         "-map", "0:a",
